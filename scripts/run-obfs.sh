343c43036b545a4cd95513619800286f86d94ebe
Edisonliiii <lifefinderedison@gmail.com>
2021-09-17 12:32:56 +0930

    update ignore

--- scripts/run-obfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ss-manager --manager-address /var/run/shadowsocks-manager.sock -c /etc/shadowsocks-libev/conf-obfs.json
-status=$?
-if [ $status -ne 0 ]; then
-  echo "first process failed! : $status"
-  exit $status
-fi
