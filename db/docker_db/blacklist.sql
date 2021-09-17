c43986b8edf27d5e13b7de282172be33735deb04
Edisonliiii <lifefinderedison@gmail.com>
2021-09-17 16:49:24 +0930

    gitignore test

--- db/docker_db/blacklist.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE IF NOT EXISTS BANNEDIP (
-  ip_addr           TEXT    UNIQUE
-);
--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ss-manager --manager-address /var/run/shadowsocks-manager.sock -c /etc/shadowsocks-libev/conf-vanilla.json
-status=$?
-if [ $status -ne 0 ]; then
-  echo "second process failed! : $status"
-  exit $status
-fi
